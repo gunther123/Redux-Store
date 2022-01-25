4bef9f430a9e229af7782ffe7054e224ba0bb518
Steven Slocum <stevesl23@hotmail.com>
2022-01-24 23:50:09 -0600

    changed global state to redux state in Detail.js

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -3,7 +3,8 @@ import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
 import Cart from '../components/Cart';
-import { useStoreContext } from '../utils/GlobalState';
+import { useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 import {
   REMOVE_FROM_CART,
   UPDATE_CART_QUANTITY,
@@ -15,7 +16,9 @@ import { idbPromise } from '../utils/helpers';
 import spinner from '../assets/spinner.gif';
 
 function Detail() {
-  const [state, dispatch] = useStoreContext();
+  const dispatch = useDispatch()
+  const selectState = state => state 
+  const state = useSelector(selectState)
   const { id } = useParams();
 
   const [currentProduct, setCurrentProduct] = useState({});
