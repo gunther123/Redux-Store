f312cd968fd3f7877d19dbf3d9d4e84204503055
Steven Slocum <stevesl23@hotmail.com>
2022-01-24 23:36:22 -0600

    removed global state to use redux with selector and dispatch

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -5,14 +5,17 @@ import { QUERY_CHECKOUT } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
 import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
-import { useStoreContext } from '../../utils/GlobalState';
+import { useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
 import './style.css';
 
 const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
-  const [state, dispatch] = useStoreContext();
+  const dispatch = useDispatch()
+  const selectState = state => state 
+  const state = useSelector(selectState)
   const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
   useEffect(() => {
@@ -48,7 +51,6 @@ const Cart = () => {
 
   function submitCheckout() {
     const productIds = [];
-
     state.cart.forEach((item) => {
       for (let i = 0; i < item.purchaseQuantity; i++) {
         productIds.push(item._id);
@@ -59,7 +61,7 @@ const Cart = () => {
       variables: { products: productIds },
     });
   }
-
+  
   if (!state.cartOpen) {
     return (
       <div className="cart-closed" onClick={toggleCart}>
@@ -69,7 +71,6 @@ const Cart = () => {
       </div>
     );
   }
-
   return (
     <div className="cart">
       <div className="close" onClick={toggleCart}>
@@ -81,10 +82,8 @@ const Cart = () => {
           {state.cart.map((item) => (
             <CartItem key={item._id} item={item} />
           ))}
-
           <div className="flex-row space-between">
             <strong>Total: ${calculateTotal()}</strong>
-
             {Auth.loggedIn() ? (
               <button onClick={submitCheckout}>Checkout</button>
             ) : (
@@ -103,5 +102,4 @@ const Cart = () => {
     </div>
   );
 };
-
 export default Cart;
